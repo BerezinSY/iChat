633fed33917d0262f712e46730b99736b586253f
Stanislav Berezin <out_berezinbu@mkb.ru>
2021-07-09 22:42:04 +0300

    Initial commit

---
+++ iChat/Launch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Stanislav BEREZIN on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
